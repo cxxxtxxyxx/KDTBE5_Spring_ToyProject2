6db083600c1bd4a3404d236a204dc74cd13be920
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-06-26 14:32:11 +0900

    chore: DB연결 및 자바 클래스 생성

--- src/main/java/BaseballManageApplication.java
+++ src/main/java/BaseballManageApplication.java
@@ -4,10 +4,15 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class BaseballManageApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Connection connection = DBConnection.getInstance();
 
+
+
+
+        connection.close();
     }
 }
