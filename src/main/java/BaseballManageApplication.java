4a09338535a156f1559c7ef24ace201030649c22
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-06-26 15:15:09 +0900

    feat: 들어오는 입력 값을 쿼리 파라미터로 파싱하는 기능 구현

--- src/main/java/BaseballManageApplication.java
+++ src/main/java/BaseballManageApplication.java
@@ -7,11 +7,32 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class BaseballManageApplication {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
         Connection connection = DBConnection.getInstance();
 
+        Scanner scanner = new Scanner(System.in);
+        String uri = scanner.nextLine();
+
+        String[] parsed = uri.split("\\?");
+
+        if (parsed.length > 2) {
+            String url = parsed[0];
+            String queryParams = parsed[1];
+
+            HashMap<String, String> paramMap = new HashMap<>();
+            String[] queryParam = queryParams.split("&");
+            for (String query : queryParam) {
+                String[] parsedQuery = query.split("=");
+                String key = parsedQuery[0];
+                String value = parsedQuery[1];
+                paramMap.put(key, value);
+            }
+            System.out.println("paramMap = " + paramMap);
+        }
 
 
+        Set<Class> classes = componentScan("./controller");
+        findUri(classes, uri);
 
         connection.close();
     }
