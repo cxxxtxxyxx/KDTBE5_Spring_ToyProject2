477ee8cb82d7fd0b8598ae7b236338421c3ddf5d
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-06-26 15:15:44 +0900

    feat: ComponentScan 메서드 구현

--- src/main/java/BaseballManageApplication.java
+++ src/main/java/BaseballManageApplication.java
@@ -1,10 +1,13 @@
+import core.Controller;
+import core.RequestMapping;
 import db.DBConnection;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
+import java.io.File;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.net.URL;
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.*;
 
 public class BaseballManageApplication {
     public static void main(String[] args) throws Exception {
@@ -35,5 +38,28 @@ public class BaseballManageApplication {
         findUri(classes, uri);
 
         connection.close();
+    }
+
+
+
+
+
+    public static Set<Class> componentScan(String pkg) throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        System.out.println("classLoader = " + classLoader);
+        Set<Class> classes = new HashSet<>();
+
+        URL packageUrl = classLoader.getResource(pkg);
+        System.out.println("packageUrl = " + packageUrl);
+        File packageDirectory = new File(packageUrl.toURI());
+        for (File file : packageDirectory.listFiles()) {
+            if (file.getName().endsWith(".class")) {
+                String className = pkg.substring(2) + "." + file.getName().replace(".class", "");
+                System.out.println(className);
+                Class cls = Class.forName(className);
+                classes.add(cls);
+            }
+        }
+        return classes;
     }
 }
