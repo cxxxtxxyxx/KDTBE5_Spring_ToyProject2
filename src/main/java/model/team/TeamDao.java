69e53f40c7bf882a92e2d2411619759eec68101a
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-06-27 14:27:52 +0900

    feat: 전체 팀 목록 조회 기능 구현

--- src/main/java/model/team/TeamDao.java
+++ src/main/java/model/team/TeamDao.java
@@ -2,9 +2,9 @@ package model.team;
 
 import db.DBConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeamDao {
 
@@ -25,6 +25,38 @@ public class TeamDao {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
+        }
+    }
+
+    public List<TeamResponseDto> findAllJoinStadium() {
+
+        List<TeamResponseDto> teamResponseDtoList = new ArrayList<>();
+        String query = "select t.id as teamId, t.name as teamName, t.created_at as teamCreatedAt, s.name as stadiumName from stadium s join team t on t.stadium_id = s.id";
+
+        try (Statement statement = connection.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(query)) {
+                while (resultSet.next()) {
+                    int teamId = resultSet.getInt("teamId");
+                    String teamName = resultSet.getString("teamName");
+                    Timestamp createdAt = resultSet.getTimestamp("teamCreatedAt");
+                    String stadiumName = resultSet.getString("stadiumName");
+
+                    TeamResponseDto teamResponseDto = TeamResponseDto.builder()
+                            .teamId(teamId)
+                            .teamName(teamName)
+                            .teamCreatedAt(createdAt)
+                            .stadiumName(stadiumName)
+                            .build();
+
+                    teamResponseDtoList.add(teamResponseDto);
+                }
+
+                return teamResponseDtoList;
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
         }
     }
 }
---
+++ src/main/java/model/team/TeamResponseDto.java
@@ -0,0 +1,21 @@
+package model.team;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Builder
+@Getter
+@Setter
+@ToString
+public class TeamResponseDto {
+
+    int teamId;
+    String teamName;
+    String stadiumName;
+    Timestamp teamCreatedAt;
+
+}
