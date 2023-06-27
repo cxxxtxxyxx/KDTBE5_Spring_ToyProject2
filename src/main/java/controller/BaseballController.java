9169e02419bf36e9e9bd1a9b49040be8f35f101b
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-06-27 13:51:00 +0900

    feat: 야구장 등록 예외 처리

--- src/main/java/controller/BaseballController.java
+++ src/main/java/controller/BaseballController.java
@@ -13,7 +13,21 @@ public class BaseballController {
 
     @RequestMapping(uri = "야구장등록")
     public boolean addStadium(Map<String, String> paramMap) {
-        return stadiumDao.add(paramMap.get("name"));
+
+        if (paramMap == null) {
+            System.out.println("올바르지 않은 요청입니다.");
+            return false;
+        }
+
+
+        if (paramMap.containsKey("name")) {
+            return stadiumDao.add(paramMap.get("name"));
+        }
+
+        System.out.println("올바르지 않은 요청입니다.");
+        return false;
+
+
     }
 
 //    @RequestMapping(uri = "야구장목록")
